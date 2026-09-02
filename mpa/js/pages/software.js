b293a89c6de31937e625adc68c4c3f1cd363bdf8
dinalaminm <mdalaminal203@gmail.com>
2026-09-02 07:18:03 +0600

    Delete mpa directory

--- mpa/js/pages/software.js
+++
@@ -1,2 +0,0 @@
-import { loadCollectionGrid } from '../common.js';
-loadCollectionGrid('software', 'softwareGrid', { type:'software', emptyText:'এখনো কোনো সফটওয়্যার যোগ করা হয়নি।' });
--- mpa/js/pages/videos.js
+++
@@ -1,2 +0,0 @@
-import { loadCollectionGrid } from '../common.js';
-loadCollectionGrid('videos', 'videoGrid', { type:'videos', emptyText:'এখনো কোনো ভিডিও যোগ করা হয়নি।' });
