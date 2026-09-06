24e0023afd5d4cd76be1d1015dced67d3738d365
dinalaminm <mdalaminal203@gmail.com>
2026-09-06 12:32:59 +0600

    Update videos.js

--- js/pages/videos.js
+++ js/pages/videos.js
@@ -1,2 +1,15 @@
-import { loadCollectionGrid } from '../common.js';
-loadCollectionGrid('videos', 'videoGrid', { type:'videos', emptyText:'এখনো কোনো ভিডিও যোগ করা হয়নি।' });
+import { loadCollectionGrid, escapeHtml } from '../common.js';
+
+const params = new URLSearchParams(window.location.search);
+const category = params.get('category');
+
+if(category){
+  const titleEl = document.getElementById('videoGridTitle');
+  if(titleEl) titleEl.innerHTML = `প্লেলিস্ট: <em>${escapeHtml(category)}</em>`;
+}
+
+loadCollectionGrid('videos', 'videoGrid', {
+  type:'videos',
+  emptyText: category ? 'এই প্লেলিস্টে এখনো কোনো ভিডিও নেই।' : 'এখনো কোনো ভিডিও যোগ করা হয়নি।',
+  filterFn: category ? (v => v.category === category) : undefined
+});
